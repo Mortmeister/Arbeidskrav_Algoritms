d573545b9aa4af49bae31a55ffbc05ab7ec5d18c
Mortmeister <morlillehaug@hotmail.com>
2026-09-16 17:05:09 +0200

    Feat: Set up phonebook contact loading
    ## Changes
    * Added Phonebook.Load() to read the supplied CSV file.
    * Added Contact[] storage to Phonebook.
    * Added a public Contacts property for accessing loaded contacts.
    * Added CSV parsing to create Contact objects.
    * Added basic testing in Program.cs to verify contacts are loaded correctly.

--- Lillehaug_Morten_Arbeidskrav1/Phonebook.cs
+++ Lillehaug_Morten_Arbeidskrav1/Phonebook.cs
@@ -3,7 +3,25 @@ namespace Lillehaug_Morten_Arbeidskrav1;
 public class Phonebook
 {
     private Contact[] _contacts = new Contact[200];
+    public Contact[] Contacts => _contacts;
     
     public enum Field { FirstName, LastName, Mobile }
     public enum SortOrder { Ascending, Descending }
+
+    public void Load(string filePath)
+    {
+        string[] lines = File.ReadAllLines(filePath);
+
+        for (int i=1; i < lines.Length; i++)
+        {
+            string[] parts = lines[i].Split(",");
+            Contact contact = new Contact(parts[0],parts[1],int.Parse(parts[2]),(parts[3]),parts[4],parts[5]);
+
+            _contacts[i-1] = contact;
+        }
+    }
+
+    
 }
+
+// For each contact, i want to assign their vallues to the _conact object. Contact [] is an array. 
--- Lillehaug_Morten_Arbeidskrav1/Program.cs
+++ Lillehaug_Morten_Arbeidskrav1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Lillehaug_Morten_Arbeidskrav1;
+using Lillehaug_Morten_Arbeidskrav1.Algorithms;
 
 class Program
 {
@@ -6,19 +7,51 @@ class Program
     {
         Console.WriteLine("=== Arbeidskrav 1 ===");
         Console.WriteLine();
-        string filePath = "../../../Data/phonebook.csv";
+        
+        Phonebook phonebook = new Phonebook();
+        phonebook.Load("../../../Data/phonebook.csv");
+
+        // 0 should be the first === Geir, second is Kristin
+        Console.WriteLine(phonebook.Contacts[0].FirstName);
+        Console.WriteLine(phonebook.Contacts[1].FirstName);
+        
+        
+        // Testing console loggin the whole csv file:
+        for (int i = 0; i < phonebook.Contacts.Length; i++)
+        {
+            Console.WriteLine(phonebook.Contacts[i].FirstName);
+        }
         
 
-        string[] lines = File.ReadAllLines(filePath);
-
-        foreach (string line in lines)
+        /*int result = LinearSearch.LinearSearchMethod(filePath, "Geir");*/
+        
+        /*foreach (string line in lines)
 
         {
 
             Console.WriteLine(line);
 
-        }
+        }*/
         
+        
+        
+
+        /*foreach (string part in parts)
+        {
+            Console.WriteLine(part);
+        }*/
+
+        
+        
+        /*Console.WriteLine(contactTest.FirstName);
+        Console.WriteLine(contactTest.LastName);
+        Console.WriteLine(contactTest.Mobile);
+        Console.WriteLine(contactTest.Birthday);
+        Console.WriteLine(contactTest.Street);
+        Console.WriteLine(contactTest.City);#1#*/
+
+        
+
         // --------------------------------
         // Question 1 - Linear Search
         // --------------------------------
@@ -26,8 +59,8 @@ class Program
         // --------------------------------
         // Question 3 - Binary Search
         // --------------------------------
-        
-        
+
+
     }
 
 }
