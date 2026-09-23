c2480364a029698eb1778fd35949e553c2a2c19b
Mortmeister <morlillehaug@hotmail.com>
2026-09-23 19:02:31 +0200

    Feat: Added tests for Bubble sort

--- Lillehaug_Morten_Arbeidskrav1/Program.cs
+++ Lillehaug_Morten_Arbeidskrav1/Program.cs
@@ -7,117 +7,160 @@ class Program
     {
         Console.WriteLine("=== Arbeidskrav 1 ===");
         Console.WriteLine();
-        
+
         Phonebook phonebook = new Phonebook();
         phonebook.Load("../../../Data/phonebook.csv");
-        
 
+        // --------------------------------
+        // Question 1 - Linear Search
+        // --------------------------------
 
-        LinearSearch.LinearSearchResult results = LinearSearch.LinearSearchMethod(
-            phonebook.Contacts,
-            Phonebook.Field.FirstName,
-            "Geir"
-        );
+        LinearSearch.LinearSearchResult results =
+            LinearSearch.LinearSearchMethod(
+                phonebook.Contacts,
+                Phonebook.Field.FirstName,
+                "Geir"
+            );
 
-        LinearSearch.LinearSearchResult results01 = LinearSearch.LinearSearchMethod(
-            phonebook.Contacts,
-            Phonebook.Field.FirstName,
-            "geir"
-        );
+        LinearSearch.LinearSearchResult results01 =
+            LinearSearch.LinearSearchMethod(
+                phonebook.Contacts,
+                Phonebook.Field.FirstName,
+                "geir"
+            );
 
-        LinearSearch.LinearSearchResult results1 = LinearSearch.LinearSearchMethod(
-            phonebook.Contacts,
-            Phonebook.Field.LastName,
-            "bjerke"
-        );
+        LinearSearch.LinearSearchResult results1 =
+            LinearSearch.LinearSearchMethod(
+                phonebook.Contacts,
+                Phonebook.Field.LastName,
+                "bjerke"
+            );
 
-        LinearSearch.LinearSearchResult results2 = LinearSearch.LinearSearchMethod(
-            phonebook.Contacts,
-            Phonebook.Field.Mobile,
-            "49572808"
-        );
+        LinearSearch.LinearSearchResult results2 =
+            LinearSearch.LinearSearchMethod(
+                phonebook.Contacts,
+                Phonebook.Field.Mobile,
+                "49572808"
+            );
 
-        LinearSearch.LinearSearchResult results3 = LinearSearch.LinearSearchMethod(
-            phonebook.Contacts,
-            Phonebook.Field.Mobile,
-            "49572808242435534343"
-        );
-        
+        LinearSearch.LinearSearchResult results3 =
+            LinearSearch.LinearSearchMethod(
+                phonebook.Contacts,
+                Phonebook.Field.Mobile,
+                "49572808242435534343"
+            );
+
         Console.WriteLine("== FIRST NAME RESULT == ");
         foreach (Contact contact in results.Results)
         {
             Console.WriteLine(contact.FirstName);
         }
-        Console.WriteLine(results.Comparisons);
-        
+
+        Console.WriteLine($"Comparisons: {results.Comparisons}");
+
         Console.WriteLine("== FIRST NAME CASE INSENSITIVE RESULT == ");
         foreach (Contact contact in results01.Results)
         {
             Console.WriteLine(contact.FirstName);
         }
-        Console.WriteLine(results01.Comparisons);
-        
+
+        Console.WriteLine($"Comparisons: {results01.Comparisons}");
+
         Console.WriteLine("== LAST NAME RESULT == ");
         foreach (Contact contact in results1.Results)
         {
             Console.WriteLine(contact.FirstName + " " + contact.LastName);
         }
-        Console.WriteLine(results01.Comparisons);
-        
+
+        Console.WriteLine($"Comparisons: {results1.Comparisons}");
+
         Console.WriteLine("== MOBILE RESULT == ");
         foreach (Contact contact in results2.Results)
         {
-            Console.WriteLine(contact.FirstName + " " + contact.LastName + " " + contact.Mobile);
+            Console.WriteLine(
+                contact.FirstName + " " +
+                contact.LastName + " " +
+                contact.Mobile
+            );
         }
-        Console.WriteLine(results2.Comparisons);
-        
-        Console.WriteLine("== Empty result == ");
+
+        Console.WriteLine($"Comparisons: {results2.Comparisons}");
+
+        Console.WriteLine("== EMPTY RESULT == ");
         foreach (Contact contact in results3.Results)
         {
-            Console.WriteLine(contact.FirstName + " " + contact.LastName + " " + contact.Mobile);
-            
+            Console.WriteLine(
+                contact.FirstName + " " +
+                contact.LastName + " " +
+                contact.Mobile
+            );
         }
-        Console.WriteLine(results3.Comparisons);
-        
- 
-        Console.WriteLine("SORT TEST");
-        Sorting.SortResult sortResult = Sorting.BubbleSort(
-            phonebook.Contacts,
+
+        Console.WriteLine($"Comparisons: {results3.Comparisons}");
+
+        // --------------------------------
+        // Question 2 - Bubble Sort
+        // --------------------------------
+
+        Phonebook testPhonebook = new Phonebook();
+        testPhonebook.Load("../../../Data/phonebook.csv");
+
+        // As supplied
+        Contact[] suppliedArray =
+            (Contact[])testPhonebook.Contacts.Clone();
+
+        Sorting.SortResult suppliedResult = Sorting.BubbleSort(
+            suppliedArray,
             Phonebook.Field.FirstName,
             Phonebook.SortOrder.Ascending
         );
 
-        Console.WriteLine($"Comparisons: {sortResult.Comparisons}");
-        Console.WriteLine($"Swaps: {sortResult.Swaps}");
+        Console.WriteLine("Bubble sort - As supplied");
+        Console.WriteLine($"Comparisons: {suppliedResult.Comparisons}");
+        Console.WriteLine($"Swaps: {suppliedResult.Swaps}");
 
-        for (int i = 0; i < 10; i++)
-        {
-            Console.WriteLine(phonebook.Contacts[i].FirstName);
-        }
- 
-        Console.WriteLine("SORT TEST");
-        Sorting.SortResult sortResult2 = Sorting.BubbleSort(
-            phonebook.Contacts,
-            Phonebook.Field.Mobile,
+        // Already sorted
+        Contact[] sortedArray =
+            (Contact[])testPhonebook.Contacts.Clone();
+
+        Sorting.BubbleSort(
+            sortedArray,
+            Phonebook.Field.FirstName,
             Phonebook.SortOrder.Ascending
         );
 
-        Console.WriteLine($"Comparisons: {sortResult2.Comparisons}");
-        Console.WriteLine($"Swaps: {sortResult2.Swaps}");
+        Sorting.SortResult sortedResult = Sorting.BubbleSort(
+            sortedArray,
+            Phonebook.Field.FirstName,
+            Phonebook.SortOrder.Ascending
+        );
 
-        for (int i = 0; i < 10; i++)
-        {
-            Console.WriteLine(phonebook.Contacts[i].Mobile);
-        }
+        Console.WriteLine("Bubble sort - Already sorted");
+        Console.WriteLine($"Comparisons: {sortedResult.Comparisons}");
+        Console.WriteLine($"Swaps: {sortedResult.Swaps}");
 
- 
-        // --------------------------------
-        // Question 1 - Linear Search
-        // --------------------------------
-        // Question 2 - Sorting
+        // Reverse sorted
+        Contact[] reverseArray =
+            (Contact[])testPhonebook.Contacts.Clone();
+
+        Sorting.BubbleSort(
+            reverseArray,
+            Phonebook.Field.FirstName,
+            Phonebook.SortOrder.Descending
+        );
+
+        Sorting.SortResult reverseResult = Sorting.BubbleSort(
+            reverseArray,
+            Phonebook.Field.FirstName,
+            Phonebook.SortOrder.Ascending
+        );
+
+        Console.WriteLine("Bubble sort - Reverse sorted");
+        Console.WriteLine($"Comparisons: {reverseResult.Comparisons}");
+        Console.WriteLine($"Swaps: {reverseResult.Swaps}");
+
         // --------------------------------
         // Question 3 - Binary Search
         // --------------------------------
     }
-
 }
